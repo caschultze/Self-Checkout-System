2944c3fa38b39d5b399ba49d167e69a41817f3ab
raymond-vong <60626161+raymond-vong@users.noreply.github.com>
2021-04-10 22:31:24 -0600

    Updated GUI for pads
    String gets printed o the Jtextfield

--- SelfCheckoutSystem - Software/SelfCheckoutSystem - Software/src/org/lsmr/software/GUIEnterPin.java
+++ SelfCheckoutSystem - Software/SelfCheckoutSystem - Software/src/org/lsmr/software/GUIEnterPin.java
@@ -4,6 +4,8 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -25,6 +27,8 @@ public class GUIEnterPin extends AbstractDevice<AbstractDeviceListener> {
 	private static JPanel extraPanel;
 	private static JPanel textPanel;
 	private static String fontname;
+	private static JTextField numberDisplay;
+	private static String pin = "";
 	private static int style;
 	private static int fontSize;
 	
@@ -68,7 +72,7 @@ public class GUIEnterPin extends AbstractDevice<AbstractDeviceListener> {
 	}
 	
 	public static void addTextfield() {
-		JTextField numberDisplay = new JTextField(16);
+		JTextField numberDisplay = new JTextField(4);
 		//numberDisplay.setFont(new Font(fontname, style, fontSize));
 		numberDisplay.setMinimumSize(new Dimension(1000, 250));
 		numberDisplay.setPreferredSize(new Dimension(1000, 500));
@@ -80,49 +84,150 @@ public class GUIEnterPin extends AbstractDevice<AbstractDeviceListener> {
 		key_1.setPreferredSize(new Dimension(10, 10));
 		padPanel.add(key_1);
 		
+		key_1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String num = e.getActionCommand();
+				pin += num;
+				numberDisplay.setText(pin);
+			}
+		});
+		
 		JButton key_2 = new JButton("2");
 		key_2.setPreferredSize(new Dimension(50, 50));
 		padPanel.add(key_2);
+		
+		key_2.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String num = e.getActionCommand();
+				pin += num;
+				numberDisplay.setText(pin);
+			}
+		});
 		
 		JButton key_3 = new JButton("3");
 		key_3.setPreferredSize(new Dimension(50, 50));
 		padPanel.add(key_3);
 		
+		key_3.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String num = e.getActionCommand();
+				pin += num;
+				numberDisplay.setText(pin);
+			}
+		});
+		
 		JButton key_4 = new JButton("4");
 		key_4.setPreferredSize(new Dimension(50, 50));
 		padPanel.add(key_4);
+		
+		key_4.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String num = e.getActionCommand();
+				pin += num;
+				numberDisplay.setText(pin);
+			}
+		});
 		
 		JButton key_5 = new JButton("5");
 		key_5.setPreferredSize(new Dimension(50, 50));
 		padPanel.add(key_5);
 		
+		key_5.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String num = e.getActionCommand();
+				pin += num;
+				numberDisplay.setText(pin);
+			}
+		});
+		
 		JButton key_6 = new JButton("6");
 		key_6.setPreferredSize(new Dimension(50, 50));
 		padPanel.add(key_6);
+		
+		key_6.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String num = e.getActionCommand();
+				pin += num;
+				numberDisplay.setText(pin);
+			}
+		});
 		
 		JButton key_7 = new JButton("7");
 		key_7.setPreferredSize(new Dimension(50, 50));
 		padPanel.add(key_7);
 		
+		key_7.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String num = e.getActionCommand();
+				pin += num;
+				numberDisplay.setText(pin);
+			}
+		});
+		
 		JButton key_8 = new JButton("8");
 		key_8.setPreferredSize(new Dimension(50, 50));
 		padPanel.add(key_8);
+		
+		key_8.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String num = e.getActionCommand();
+				pin += num;
+				numberDisplay.setText(pin);
+			}
+		});
 		
 		JButton key_9 = new JButton("9");
 		key_9.setPreferredSize(new Dimension(50, 50));
 		padPanel.add(key_9);
 		
+		key_9.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String num = e.getActionCommand();
+				pin += num;
+				numberDisplay.setText(pin);
+			}
+		});
+		
 		JButton key_0 = new JButton("0");
 		key_0.setPreferredSize(new Dimension(50, 50));
 		padPanel.add(key_0);
+		
+		key_0.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String num = e.getActionCommand();
+				pin += num;
+				numberDisplay.setText(pin);
+			}
+		});
 		
 		JButton key_back = new JButton("Backspace");
 		key_back.setPreferredSize(new Dimension(50, 50));
 		padPanel.add(key_back);
 		
+		key_back.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String num = e.getActionCommand();
+				pin = pin.substring(0, pin.length()-1);
+				numberDisplay.setText(pin);
+			}
+		});
+		
+		
 		JButton key_Enter = new JButton("Enter");
 		key_Enter.setPreferredSize(new Dimension(100, 50));
 		padPanel.add(key_Enter);
+		
 		
 		fontname = key_1.getFont().getName();
 		style = key_1.getFont().getStyle();
@@ -140,6 +245,10 @@ public class GUIEnterPin extends AbstractDevice<AbstractDeviceListener> {
 		key_0.setFont(new Font(fontname, style, fontSize));
 		key_Enter.setFont(new Font(fontname, style, fontSize));
 		key_back.setFont(new Font(fontname, style, fontSize));
+	}
+	
+	public static String returnpin () {
+		return pin;
 	}
 	
 	public static void addExtraButtons () {
--- SelfCheckoutSystem - Software/SelfCheckoutSystem - Software/src/org/lsmr/software/GUIMembershipLogIn.java
+++ SelfCheckoutSystem - Software/SelfCheckoutSystem - Software/src/org/lsmr/software/GUIMembershipLogIn.java
@@ -4,6 +4,9 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -24,6 +27,8 @@ public class GUIMembershipLogIn extends AbstractDevice<AbstractDeviceListener> {
 	private static JPanel extraPanel;
 	private static JPanel textPanel;
 	private static String fontname;
+	private static JTextField numberDisplay;
+	private static String membershipID = "";
 	private static int style;
 	private static int fontSize;
 	
@@ -67,7 +72,7 @@ public class GUIMembershipLogIn extends AbstractDevice<AbstractDeviceListener> {
 	}
 	
 	public static void addTextfield() {
-		JTextField numberDisplay = new JTextField(16);
+		numberDisplay = new JTextField(16);
 		//numberDisplay.setFont(new Font(fontname, style, fontSize));
 		numberDisplay.setMinimumSize(new Dimension(1000, 250));
 		numberDisplay.setPreferredSize(new Dimension(1000, 500));
@@ -79,49 +84,149 @@ public class GUIMembershipLogIn extends AbstractDevice<AbstractDeviceListener> {
 		key_1.setPreferredSize(new Dimension(10, 10));
 		padPanel.add(key_1);
 		
+		key_1.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String num = e.getActionCommand();
+				membershipID += num;
+				numberDisplay.setText(membershipID);
+			}
+		});
+		
 		JButton key_2 = new JButton("2");
 		key_2.setPreferredSize(new Dimension(50, 50));
 		padPanel.add(key_2);
+		
+		key_2.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String num = e.getActionCommand();
+				membershipID += num;
+				numberDisplay.setText(membershipID);
+			}
+		});
 		
 		JButton key_3 = new JButton("3");
 		key_3.setPreferredSize(new Dimension(50, 50));
 		padPanel.add(key_3);
 		
+		key_3.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String num = e.getActionCommand();
+				membershipID += num;
+				numberDisplay.setText(membershipID);
+			}
+		});
+		
 		JButton key_4 = new JButton("4");
 		key_4.setPreferredSize(new Dimension(50, 50));
 		padPanel.add(key_4);
+		
+		key_4.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String num = e.getActionCommand();
+				membershipID += num;
+				numberDisplay.setText(membershipID);
+			}
+		});
 		
 		JButton key_5 = new JButton("5");
 		key_5.setPreferredSize(new Dimension(50, 50));
 		padPanel.add(key_5);
 		
+		key_5.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String num = e.getActionCommand();
+				membershipID += num;
+				numberDisplay.setText(membershipID);
+			}
+		});
+		
 		JButton key_6 = new JButton("6");
 		key_6.setPreferredSize(new Dimension(50, 50));
 		padPanel.add(key_6);
+		
+		key_6.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String num = e.getActionCommand();
+				membershipID += num;
+				numberDisplay.setText(membershipID);
+			}
+		});
 		
 		JButton key_7 = new JButton("7");
 		key_7.setPreferredSize(new Dimension(50, 50));
 		padPanel.add(key_7);
 		
+		key_7.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String num = e.getActionCommand();
+				membershipID += num;
+				numberDisplay.setText(membershipID);
+			}
+		});
+		
 		JButton key_8 = new JButton("8");
 		key_8.setPreferredSize(new Dimension(50, 50));
 		padPanel.add(key_8);
+		
+		key_8.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String num = e.getActionCommand();
+				membershipID += num;
+				numberDisplay.setText(membershipID);
+			}
+		});
 		
 		JButton key_9 = new JButton("9");
 		key_9.setPreferredSize(new Dimension(50, 50));
 		padPanel.add(key_9);
 		
+		key_9.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String num = e.getActionCommand();
+				membershipID += num;
+				numberDisplay.setText(membershipID);
+			}
+		});
+		
 		JButton key_0 = new JButton("0");
 		key_0.setPreferredSize(new Dimension(50, 50));
 		padPanel.add(key_0);
+		
+		key_0.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String num = e.getActionCommand();
+				membershipID += num;
+				numberDisplay.setText(membershipID);
+			}
+		});
 		
 		JButton key_back = new JButton("Backspace");
 		key_back.setPreferredSize(new Dimension(50, 50));
 		padPanel.add(key_back);
 		
+		key_back.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				String num = e.getActionCommand();
+				membershipID = membershipID.substring(0, membershipID.length()-1);
+				numberDisplay.setText(membershipID);
+			}
+		});
+		
 		JButton key_Enter = new JButton("Enter");
 		key_Enter.setPreferredSize(new Dimension(100, 50));
 		padPanel.add(key_Enter);
+		
 		
 		fontname = key_1.getFont().getName();
 		style = key_1.getFont().getStyle();
@@ -139,6 +244,10 @@ public class GUIMembershipLogIn extends AbstractDevice<AbstractDeviceListener> {
 		key_0.setFont(new Font(fontname, style, fontSize));
 		key_Enter.setFont(new Font(fontname, style, fontSize));
 		key_back.setFont(new Font(fontname, style, fontSize));
+	}
+	
+	public static String returnMembership () {
+		return membershipID;
 	}
 	
 	public static void addExtraButtons () {
