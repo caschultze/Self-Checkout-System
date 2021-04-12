005f6d131617cfe32bc42170932d98dd1a2db5b3
dohkhanh <do.h.khanh@gmail.com>
2021-04-12 15:46:16 -0600

    Added a border label to hidden panel

--- SelfCheckoutSystem - Software/SelfCheckoutSystem - Software/src/org/lsmr/software/AdminGUI.java
+++ SelfCheckoutSystem - Software/SelfCheckoutSystem - Software/src/org/lsmr/software/AdminGUI.java
@@ -14,6 +14,7 @@ import java.math.BigDecimal;
 import java.util.Currency;
 import java.util.Locale;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -126,6 +127,8 @@ public class AdminGUI extends MainGUI {
         // (Buttons: Refill Paper, Refill Ink, Empty Coin Storage, Empty Banknote storage, Refill Coin Storage, Refill Banknote storage)
         
         // Create Components -------------------------------------------------
+        
+        hiddenPanel.setBorder(BorderFactory.createTitledBorder("STATION MUST BE BLOCKED TO ACCESS"));
         
         paperBtn = new JButton("Add Paper");
         paperBtn.setFont(new Font("Arial", Font.PLAIN, 30));
